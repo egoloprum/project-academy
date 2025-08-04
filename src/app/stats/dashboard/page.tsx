c559d0bcb962f2fccc28174e78b46ac07516cf05
egoloprum <bayraa093@gmail.com>
2025-08-04 19:11:24 +0300

    ui: add all time stats widget in dashboard

--- src/app/stats/dashboard/page.tsx
+++ src/app/stats/dashboard/page.tsx
@@ -1,5 +1,29 @@
-const page = ({}) => {
-  return <div>page</div>
+import { DashboardGoBackBtn } from '@/features/(stats)'
+import { isUserType } from '@/shared/lib'
+import { DashboardAlltimeWidget } from '@/widgets/(stats)'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+const page = async ({}) => {
+  const cookieStore = await cookies()
+  const auth = cookieStore.get('authUser')
+
+  const userType = auth && isUserType(auth.value) ? auth.value : undefined
+
+  if (!userType) {
+    redirect('/login')
+  }
+
+  if (userType === 'intern' || userType === 'mentor') {
+    redirect('/profile')
+  }
+
+  return (
+    <main className="bg-black min-h-screen text-white px-6 sm:px-12 md:px-24 py-10 flex flex-col gap-8">
+      <DashboardGoBackBtn />
+      <DashboardAlltimeWidget />
+    </main>
+  )
 }
 
 export default page
--- src/widgets/(stats)/index.ts
+++ src/widgets/(stats)/index.ts
@@ -1,1 +1,3 @@
 export * from './ui/statsTable'
+
+export * from './ui/dashboardAlltimeWidget'

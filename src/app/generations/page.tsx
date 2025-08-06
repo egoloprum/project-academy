5ffe28b4b2b1b4bd1050551e1e5c488b80ba6c8e
egoloprum <bayraa093@gmail.com>
2025-08-06 15:48:54 +0300

    feature: add button that redirects to create new generation page

---
+++ src/app/generations/page.tsx
@@ -0,0 +1,28 @@
+import { GenerationCreateNewBtn } from '@/features/(generations)'
+import { isUserType } from '@/shared/lib'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+const page = async ({}) => {
+  const cookieStore = await cookies()
+  const auth = cookieStore.get('authUser')
+
+  const userType = auth && isUserType(auth.value) ? auth.value : undefined
+
+  if (!userType) {
+    redirect('/login')
+  }
+
+  if (userType === 'intern' || userType === 'mentor') {
+    redirect('/profile')
+  }
+
+  return (
+    <main className="bg-black min-h-screen text-white px-6 sm:px-12 md:px-24 py-10 flex flex-col gap-4">
+      <p className="text-2xl font-bold mb-8">Список всех наборов</p>
+      <GenerationCreateNewBtn />
+    </main>
+  )
+}
+
+export default page
